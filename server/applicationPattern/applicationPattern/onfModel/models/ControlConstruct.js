b033e96d1141ceb1129999365f7b57605dc444bd
Prathiba Jeevan <62344562+PrathibaJee@users.noreply.github.com>
2023-05-17 07:02:42 +0200

    Revert "issue-648 : added more unittest"

--- server/applicationPattern/applicationPattern/onfModel/models/ControlConstruct.js
+++ server/applicationPattern/applicationPattern/onfModel/models/ControlConstruct.js
@@ -16,97 +16,160 @@ class ControlConstruct {
 
   /**
    * @description This function returns the forwarding-domain list entries from the core-model-1-4:control-construct
-   * @returns {Promise<Array>} ForwardingDomain List
+   * @returns {promise} returns ForwardingDomain List.
    **/
   static async getForwardingDomainListAsync() {
-    return await fileOperation.readFromDatabaseAsync(onfPaths.FORWARDING_DOMAIN);
+    return new Promise(async function (resolve, reject) {
+      try {
+        let forwardingDomainList = await fileOperation.readFromDatabaseAsync(onfPaths.FORWARDING_DOMAIN);
+        resolve(forwardingDomainList);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   /**
    * @description This function returns the logical-termination-point list entries from the core-model-1-4:control-construct that 
-   * matches the layerProtocolName. If layerProtocolName is undefined, it returns all LTPs.
-   * @param {String} layerProtocolName The value can be either undefined or any one of the LayerProtocol.layerProtocolNameEnum
-   * @returns {Promise<Array>} LogicalTerminationPoint instance List
+   * matches the layerProtocolName
+   * @param {String} layerProtocolName : The vaue can be either undefined or any one of the LayerProtocol.layerProtocolNameEnum
+   * @returns {promise} returns LogicalTerminationPoint instance List.
    **/
   static async getLogicalTerminationPointListAsync(layerProtocolName) {
-    let logicalTerminationPointList = await fileOperation.readFromDatabaseAsync(
-      onfPaths.LOGICAL_TERMINATION_POINT);
-    if (!layerProtocolName) {
-      return logicalTerminationPointList;
-    }
-    let filteredLogicalTerminationPointList = [];
-    if (layerProtocolName) {
-      for (let logicalTerminationPoint of logicalTerminationPointList) {
-        let layerProtocolList = logicalTerminationPoint[onfAttributes.LOGICAL_TERMINATION_POINT.LAYER_PROTOCOL];
-        let layerProtocol = layerProtocolList[0];
-        let _layerProtocolName = layerProtocol[onfAttributes.LAYER_PROTOCOL.LAYER_PROTOCOL_NAME];
-        if (_layerProtocolName === layerProtocolName) {
-          filteredLogicalTerminationPointList.push(logicalTerminationPoint);
+    return new Promise(async function (resolve, reject) {
+      let filteredLogicalTerminationPointList = [];
+      try {
+        let logicalTerminationPointList = await fileOperation.readFromDatabaseAsync(
+          onfPaths.LOGICAL_TERMINATION_POINT);
+        if (layerProtocolName) {
+          for (let i = 0; i < logicalTerminationPointList.length; i++) {
+            let logicalTerminationPoint = logicalTerminationPointList[i];
+            let layerProtocolList = logicalTerminationPoint[onfAttributes.LOGICAL_TERMINATION_POINT.LAYER_PROTOCOL];
+            let layerProtocol = layerProtocolList[0]
+            let _layerProtocolName = layerProtocol[onfAttributes.LAYER_PROTOCOL.LAYER_PROTOCOL_NAME];
+            if (_layerProtocolName == layerProtocolName) {
+              filteredLogicalTerminationPointList.push(logicalTerminationPoint);
+            }
+          }
+          resolve(filteredLogicalTerminationPointList);
+        } else {
+          resolve(logicalTerminationPointList);
         }
+      } catch (error) {
+        reject(error);
       }
-      return filteredLogicalTerminationPointList;
-    }
+    });
   }
 
   /**
    * @description This function returns the logical-termination-point instance from the core-model-1-4:control-construct/
    * logical-termination-point that matches the logicalTerminationPointUuid
    * @param {String} logicalTerminationPointUuid : the value should be a valid string in the pattern '-\d+-\d+-\d+-(http|tcp|op)-(s|c)-\d{4}$'
-   * @returns {Promise<Object|undefined>} LogicalTerminationPoint
+   * @returns {promise} returns {undefined | LogicalTerminationPoint}
    **/
-  static async getLogicalTerminationPointAsync(logicalTerminationPointUuid) {
-    let logicalTerminationPointList = await fileOperation.readFromDatabaseAsync(
-      onfPaths.LOGICAL_TERMINATION_POINT
-    );
-    if (!logicalTerminationPointList) {
-      return undefined;
-    }
-    return logicalTerminationPointList.find(ltp => ltp[onfAttributes.GLOBAL_CLASS.UUID] === logicalTerminationPointUuid);
+  static getLogicalTerminationPointAsync(logicalTerminationPointUuid) {
+    return new Promise(async function (resolve, reject) {
+      let logicalTerminationPoint;
+      try {
+        let logicalTerminationPointList = await fileOperation.readFromDatabaseAsync(
+          onfPaths.LOGICAL_TERMINATION_POINT
+        );
+        if (logicalTerminationPointList != undefined) {
+          for (let i = 0; i < logicalTerminationPointList.length; i++) {
+            let _logicalTerminationPoint = logicalTerminationPointList[i];
+            let _logicalTerminationPointUuid = _logicalTerminationPoint[onfAttributes.GLOBAL_CLASS.UUID];
+            if (_logicalTerminationPointUuid == logicalTerminationPointUuid) {
+              logicalTerminationPoint = _logicalTerminationPoint;
+            }
+          }
+        }
+        resolve(logicalTerminationPoint);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
 
   /**
    * @description This function returns the profile-collection from core-model-1-4:control-construct
-   * @returns {Promise} profile-collection
+   * @returns {promise} returns {undefined | profileCollection}
    **/
   static async getProfileCollectionAsync() {
-    return await fileOperation.readFromDatabaseAsync(onfPaths.PROFILE_COLLECTION);
+    return new Promise(async function (resolve, reject) {
+      let profileCollection;
+      try {
+        profileCollection = await fileOperation.readFromDatabaseAsync(
+          onfPaths.PROFILE_COLLECTION
+        );
+        resolve(profileCollection);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   /**
    * @description This function returns the current value of the attribute core-model-1-4:control-construct/uuid
-   * @returns {Promise<String|undefined>} UUID
+   * @returns {promise} returns {undefined | uuid}
    **/
   static async getUuidAsync() {
-    return await fileOperation.readFromDatabaseAsync(onfPaths.CONTROL_CONSTRUCT_UUID);
+    return new Promise(async function (resolve, reject) {
+      let uuid;
+      try {
+        uuid = await fileOperation.readFromDatabaseAsync(
+          onfPaths.CONTROL_CONSTRUCT_UUID
+        );
+        resolve(uuid);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   /**
    * @description This function adds a logical-termination-point instance to the core-model-1-4:control-construct/logical-termination-point
-   * @param {Object} logicalTerminationPoint an instance of the LogicalTerminationPoint
-   * @returns {Promise<Boolean>}
+   * @param {String} logicalTerminationPoint an instance of the LogicalTerminationPoint
+   * @returns {promise} returns {true|false}
    **/
-  static async addLogicalTerminationPointAsync(logicalTerminationPoint) {
-    logicalTerminationPoint = onfFormatter.modifyJsonObjectKeysToKebabCase(logicalTerminationPoint);
-    return await fileOperation.writeToDatabaseAsync(
-      onfPaths.LOGICAL_TERMINATION_POINT,
-      logicalTerminationPoint,
-      true);
+  static addLogicalTerminationPointAsync(logicalTerminationPoint) {
+    return new Promise(async function (resolve, reject) {
+      let isCreated = false;
+      try {
+        logicalTerminationPoint = onfFormatter.modifyJsonObjectKeysToKebabCase(logicalTerminationPoint);
+        isCreated = await fileOperation.writeToDatabaseAsync(
+          onfPaths.LOGICAL_TERMINATION_POINT,
+          logicalTerminationPoint,
+          true);
+        resolve(isCreated);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   /**
    * @description This function deletes a logical-termination-point instance that matches the uuid argument from the 
    * core-model-1-4:control-construct/logical-termination-point
    * @param {String} logicalTerminationPointUuid : the value should be a valid string in the pattern '-\d+-\d+-\d+-(http|tcp|op)-(s|c)-\d{4}$'
-   * @returns {Promise<Boolean>}
+   * @returns {promise} returns {true|false}
    **/
-  static async deleteLogicalTerminationPointAsync(logicalTerminationPointUuid) {
-    let logicalTerminationPointPath = onfPaths.LOGICAL_TERMINATION_POINT + "=" + logicalTerminationPointUuid;
-    return await fileOperation.deletefromDatabaseAsync(
-      logicalTerminationPointPath,
-      logicalTerminationPointUuid,
-      true);
+  static deleteLogicalTerminationPointAsync(logicalTerminationPointUuid) {
+    return new Promise(async function (resolve, reject) {
+      let isDeleted = false;
+      try {
+        let logicalTerminationPointPath = onfPaths.LOGICAL_TERMINATION_POINT + "=" + logicalTerminationPointUuid
+        isDeleted = await fileOperation.deletefromDatabaseAsync(
+          logicalTerminationPointPath, 
+          logicalTerminationPointUuid, 
+          true);
+        resolve(isDeleted);
+      } catch (error) {
+        reject(false);
+      }
+    });
   }
+
 }
 
 module.exports = ControlConstruct;
--- server/applicationPattern/applicationPattern/onfModel/models/ForwardingDomain.js
+++ server/applicationPattern/applicationPattern/onfModel/models/ForwardingDomain.js
@@ -1,4 +1,3 @@
-// @ts-check
 /**
  * The ForwardingDomain (FD) class models the component that represents a forwarding capability that provides the opportunity 
  * to enable forwarding between different microservices. 
@@ -16,93 +15,145 @@ class ForwardingDomain {
     /**
      * @description This function returns a list that consists of the all the ForwardingConstruct instances 
      * in the /core-model-1-4:control-construct/forwarding-domain list
-     * @returns {Promise<Array>} ForwardingConstructList
+     * @returns {promise} list {[] | ForwardingConstructList}
      **/
     static async getForwardingConstructListAsync() {
-        let forwardingConstructList = [];
-        let forwardingDomainList = await controlConstruct.getForwardingDomainListAsync();
-        for (let forwardingDomain of forwardingDomainList) {
-            let fcs = forwardingDomain[onfAttributes.FORWARDING_DOMAIN.FORWARDING_CONSTRUCT];
-            forwardingConstructList.push(fcs);
-        }
-        return forwardingConstructList.flat();
+        return new Promise(async function (resolve, reject) {
+            let forwardingConstructList = [];
+            try {
+                let ForwardingDomainList = await controlConstruct.getForwardingDomainListAsync();
+                for (let i = 0; i < ForwardingDomainList.length; i++) {
+                    let forwardingDomain = ForwardingDomainList[i];
+                    let _forwardingConstructList = forwardingDomain[
+                        onfAttributes.FORWARDING_DOMAIN.FORWARDING_CONSTRUCT];
+                    for (let j = 0; j < _forwardingConstructList.length; j++) {
+                        let forwardingConstruct = _forwardingConstructList[j];
+                        forwardingConstructList.push(forwardingConstruct);
+                    }
+                }
+                resolve(forwardingConstructList);
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 
     /**
      * @description This function returns a ForwardingConstruct instance lists that consists of the fc-port/LogicalTerminationPoint 
      * and fcPortDirection.
-     * @param {String} ltpUuid uuid of a logical-termination-point instance. It should be a valid string in the
+     * @param fcPortLogicalTerminationPoint : uuid of a logical-termination-point instance. It should be a valid string in the 
      * pattern '-\d+-\d+-\d+-op-(s|c)-\d{4}$'.
-     * @param {Enumerator} fcPortDirection the value can be any one of the ForwardingConstruct.FcPort.portDirectionEnum
-     * @returns {Promise<Array>} filtered ForwardingConstructList
+     * @param fcPortDirection : the vaue can be any one of the ForwardingConstruct.FcPort.portDirectionEnum
+     * @returns {promise} list {undefined | filtered ForwardingConstructList}
      **/
-    static async getForwardingConstructListForTheFcPortAsync(ltpUuid, fcPortDirection) {
-        let filteredForwardingConstructList = [];
-        let forwardingConstructList = await ForwardingDomain.getForwardingConstructListAsync();
-        for (let forwardingConstruct of forwardingConstructList) {
-            let fcPortList = forwardingConstruct[onfAttributes.FORWARDING_CONSTRUCT.FC_PORT];
-            for (let fcPort of fcPortList) {
-                let portDirection = fcPort[onfAttributes.FC_PORT.PORT_DIRECTION];
-                if (fcPortDirection === portDirection) {
-                    let _fcPortLogicalTerminationPoint = fcPort[onfAttributes.FC_PORT.LOGICAL_TERMINATION_POINT];
-                    if (_fcPortLogicalTerminationPoint === ltpUuid) {
-                        filteredForwardingConstructList.push(forwardingConstruct);
+    static async getForwardingConstructListForTheFcPortAsync(fcPortLogicalTerminationPoint, fcPortDirection) {
+        return new Promise(async function (resolve, reject) {
+            let filteredForwardingConstructList = [];
+            try {
+                let forwardingConstructList = await ForwardingDomain.getForwardingConstructListAsync();
+                for (let i = 0; i < forwardingConstructList.length; i++) {
+                    let forwardingConstruct = forwardingConstructList[i];
+                    let fcPortList = forwardingConstruct[onfAttributes.FORWARDING_CONSTRUCT.FC_PORT];
+                    for (let j = 0; j < fcPortList.length; j++) {
+                        let fcPort = fcPortList[j];
+                        let portDirection = fcPort[onfAttributes.FC_PORT.PORT_DIRECTION];
+                        if (fcPortDirection == portDirection) {
+                            let _fcPortLogicalTerminationPoint = fcPort[onfAttributes.FC_PORT.LOGICAL_TERMINATION_POINT];
+                            if (_fcPortLogicalTerminationPoint == fcPortLogicalTerminationPoint) {
+                                filteredForwardingConstructList.push(forwardingConstruct);
+                            }
+                        }
                     }
                 }
+                resolve(filteredForwardingConstructList);
+            } catch (error) {
+                reject(error);
             }
-        }
-        return filteredForwardingConstructList;
+        });
     }
 
     /**
      * @description This function returns a ForwardingConstruct instances from the /core-model-1-4:control-construct/forwarding-domain 
      * list that matches the argument forwardingConstructUuid.
-     * @param {String} forwardingConstructUuid : the value should be a valid string in the pattern '-\d+-\d+-\d+-op-fc-\d{4}$'
-     * @returns {Promise<Object|undefined>} ForwardingConstruct instance
+     * @param {string} forwardingConstructUuid : the value should be a valid string in the pattern '-\d+-\d+-\d+-op-fc-\d{4}$'
+     * @returns {promise} object {undefined | ForwardingConstruct instance}
      **/
     static async getForwardingConstructAsync(forwardingConstructUuid) {
-        let forwardingConstructList = await ForwardingDomain.getForwardingConstructListAsync();
-        return forwardingConstructList.find(fc => fc[onfAttributes.GLOBAL_CLASS.UUID] === forwardingConstructUuid);
+        return new Promise(async function (resolve, reject) {
+            try {
+                let forwardingConstruct;
+                let forwardingConstructList = await ForwardingDomain.getForwardingConstructListAsync();
+                for (let i = 0; i < forwardingConstructList.length; i++) {
+                    let _forwardingConstruct = forwardingConstructList[i];
+                    let _forwardingConstructUuid = _forwardingConstruct[onfAttributes.GLOBAL_CLASS.UUID];
+                    if (_forwardingConstructUuid == forwardingConstructUuid) {
+                        forwardingConstruct = _forwardingConstruct;
+                    }
+                }
+                resolve(forwardingConstruct);
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 
     /**
      * @description This function returns a ForwardingConstruct instances from the /core-model-1-4:control-construct/forwarding-domain 
      * list that matches the argument forwardingName
-     * @param {String} forwardingName : name/value-name/ForwardingName of the forwarding-construct
-     * @returns {Promise<Object|undefined>} ForwardingConstruct instance
+     * @param {string} forwardingName : name/value-name/ForwardingName of the forwarding-construct
+     * @returns {promise} object {undefined | ForwardingConstruct instance}
      **/
     static async getForwardingConstructForTheForwardingNameAsync(forwardingName) {
-        let forwardingConstructList = await ForwardingDomain.getForwardingConstructListAsync();
-        for (let _forwardingConstruct of forwardingConstructList) {
-            let nameList = _forwardingConstruct[onfAttributes.FORWARDING_CONSTRUCT.NAME];
-            for (let name of nameList) {
-                let valueName = name[onfAttributes.NAME.VALUE_NAME];
-                let value = name[onfAttributes.NAME.VALUE];
-                if (valueName === "ForwardingName" && value === forwardingName) {
-                    return _forwardingConstruct;
+        return new Promise(async function (resolve, reject) {
+            try {
+                let forwardingConstruct;
+                let forwardingConstructList = await ForwardingDomain.getForwardingConstructListAsync();
+                for (let i = 0; i < forwardingConstructList.length; i++) {
+                    let _forwardingConstruct = forwardingConstructList[i];
+                    let nameList = _forwardingConstruct[onfAttributes.FORWARDING_CONSTRUCT.NAME];
+                    for (let k = 0; k < nameList.length; k++) {
+                        let valueName = nameList[k][onfAttributes.NAME.VALUE_NAME];
+                        let value = nameList[k][onfAttributes.NAME.VALUE];
+                        if (valueName == "ForwardingName" && value == forwardingName) {
+                            forwardingConstruct = _forwardingConstruct;
+                        }
+                    }
                 }
+                resolve(forwardingConstruct);
+            } catch (error) {
+                reject(error);
             }
-        }
-        return undefined;
+        });
     }
+
 
     /**
      * @description This function returns the uuid of the /core-model-1-4:control-construct/forwarding-domain that 
      * matches the forwardingConstructUuid
-     * @param {String} forwardingConstructUuid : the value should be a valid string in the pattern '-\d+-\d+-\d+-op-fc-\d{4}$'
-     * @returns {Promise<String|undefined>} UUID of forwarding-domain
+     * @param {string} forwardingConstructUuid : the value should be a valid string in the pattern '-\d+-\d+-\d+-op-fc-\d{4}$'
+     * @returns {promise} string {undefined | uuid of forwarding-domain}
      **/
     static async getForwardingDomainUuidAsync(forwardingConstructUuid) {
-        let forwardingDomainList = await controlConstruct.getForwardingDomainListAsync();
-        for (let forwardingDomain of forwardingDomainList) {
-            let forwardingConstructList = forwardingDomain[onfAttributes.FORWARDING_DOMAIN.FORWARDING_CONSTRUCT];
-            for (let forwardingConstruct of forwardingConstructList) {
-                let _forwardingConstructUuid = forwardingConstruct[onfAttributes.GLOBAL_CLASS.UUID];
-                if (_forwardingConstructUuid === forwardingConstructUuid) {
-                    return forwardingDomain[onfAttributes.GLOBAL_CLASS.UUID];
+        return new Promise(async function (resolve, reject) {
+            try {
+                let forwardingDomainUuid;
+                let ForwardingDomainList = await controlConstruct.getForwardingDomainListAsync();
+                for (let i = 0; i < ForwardingDomainList.length; i++) {
+                    let forwardingDomain = ForwardingDomainList[i];
+                    let forwardingConstructList = forwardingDomain[onfAttributes.FORWARDING_DOMAIN.FORWARDING_CONSTRUCT];
+                    for (let j = 0; j < forwardingConstructList.length; j++) {
+                        let forwardingConstruct = forwardingConstructList[j];
+                        let _forwardingConstructUuid = forwardingConstruct[onfAttributes.GLOBAL_CLASS.UUID];
+                        if (_forwardingConstructUuid == forwardingConstructUuid) {
+                            forwardingDomainUuid = forwardingDomain[onfAttributes.GLOBAL_CLASS.UUID];
+                        }
+                    }
                 }
+                resolve(forwardingDomainUuid);
+            } catch (error) {
+                reject(error);
             }
-        }
+        });
     }
 }
 
