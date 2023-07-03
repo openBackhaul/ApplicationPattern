9e4da9d04ffff4fa03599d3bd4c0b606775ee598
Dana Sunalova <dana@paxet.io>
2023-07-03 10:16:28 +0200

    Added back wrongly deleted tests.
    Signed-off-by: Dana Sunalova <dana@paxet.io>

--- server/applicationPattern/applicationPattern/onfModel/models/FcPort.test.js
+++ server/applicationPattern/applicationPattern/onfModel/models/FcPort.test.js
@@ -1,4 +1,64 @@
-const FcPort = require("./FcPort");
+const FcPort = require('./FcPort')
+const fileOperation = require('../../databaseDriver/JSONDriver');
+const ForwardingDomain = require('./ForwardingDomain');
+
+jest.mock('../../databaseDriver/JSONDriver');
+jest.mock('./ForwardingDomain');
+
+beforeEach(() => {
+  jest.spyOn(fileOperation, 'writeToDatabaseAsync').mockImplementation(() => true);
+})
+
+const fc =  {
+  "fc-port" : [
+    {
+      "local-id" : "000",
+      "port-direction" : "core-model-1-4:PORT_DIRECTION_TYPE_MANAGEMENT",
+      "logical-termination-point" : "alt-2-0-1-op-s-bm-001"
+    },
+    {
+      "local-id" : "200",
+      "port-direction" : "core-model-1-4:PORT_DIRECTION_TYPE_OUTPUT",
+      "logical-termination-point" : "alt-2-0-1-op-c-bm-ro-2-0-1-003"
+    },
+    {
+      "local-id" : "201",
+      "port-direction" : "core-model-1-4:PORT_DIRECTION_TYPE_OUTPUT",
+      "logical-termination-point" : "alt-2-0-1-op-c-bm-ro-2-0-1-004"
+    }
+  ]
+};
+
+const fcZero =  {
+  "fc-port" : [
+    {
+      "local-id" : "000",
+      "port-direction" : "core-model-1-4:PORT_DIRECTION_TYPE_MANAGEMENT",
+      "logical-termination-point" : "alt-2-0-1-op-s-bm-001"
+    }
+  ]
+};
+
+test("generateNextLocalId", () => {
+  expect(FcPort.generateNextLocalId(fc)).toBe("202");
+  expect(FcPort.generateNextLocalId(fcZero)).toBe("200");
+});
+
+test("getLocalId", () => {
+  expect(FcPort.getLocalId(fc, "alt-2-0-1-op-s-bm-001")).toBe("000");
+  expect(FcPort.getLocalId(fc, "alt-2-0-1-op-c-bm-ro-2-0-1-003")).toBe("200");
+  expect(FcPort.getLocalId(fc, "alt-2-0-1-op-s-bm-020")).toBeUndefined();
+});
+
+test("setLogicalTerminationPointAsync", async () => {
+  jest.spyOn(ForwardingDomain, 'getForwardingDomainUuidAsync').mockImplementation(() => "fw-domain-uuid");
+  const res = await FcPort.setLogicalTerminationPointAsync("alt-2-0-1-op-fc-bm-107", "201", "alt-2-0-1-op-c-bm-ro-2-0-1-005");
+  expect(fileOperation.writeToDatabaseAsync).toBeCalledWith(
+    "/core-model-1-4:control-construct/forwarding-domain=fw-domain-uuid/forwarding-construct=alt-2-0-1-op-fc-bm-107/fc-port=201/logical-termination-point",
+    "alt-2-0-1-op-c-bm-ro-2-0-1-005", false
+  );
+  expect(res).toBeTruthy();
+});
 
 test("isOperationOfFcPortType", async () => {
     const fc = {
@@ -32,4 +92,8 @@ test("isOperationOfFcPortType", async () => {
     expect(FcPort.isOperationOfFcPortType(
         fc, "ol-2-0-1-op-c-bm-alt-1-0-0-005", FcPort.portDirectionEnum.OUTPUT)
     ).toBeTruthy();
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
 });
